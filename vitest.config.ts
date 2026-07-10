3d9ccdf99b1c5370716cc7ff47fec66a6b28da71
avromy <avromy@gmail.com>
2026-07-10 19:29:25 -0400

    Use official React plugin in Vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,8 @@
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  esbuild: {
-    tsconfigRaw: {
-      compilerOptions: {
-        jsx: 'react-jsx',
-      },
-    },
-  } as never,
+  plugins: [react()],
   test: {
     environment: 'node',
     globals: true,
